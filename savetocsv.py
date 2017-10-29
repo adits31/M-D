eadae3d54736eb8e2f9d5dec8e97e7fcf07f4641
Tim Toombs <timothytoombs@berkeley.edu>
2017-10-28 19:04:53 -0700

    csv save

---
+++ savetocsv.py
@@ -0,0 +1,22 @@
+import lyricwikia as lw
+import time
+import pandas
+
+def saveCSV(songArtistCSV):
+	df = pandas.read_csv(songArtistCSV)
+	nf = 0
+	lyrArr = []
+	for index, row in df.iterrows():
+		try:
+			lyrics = lw.get_lyrics((row["Artist"]+";")[:row["Artist"].find(";")],row["Song"])
+			lyrArr += [lyrics]
+		except Exception as e:
+			print(row["Song"], " not found.")
+			nf+=1
+			lyrArr += [-1]
+		print("Song Index ", index)
+	print("Number of Songs not Found: ", nf)
+	df["lyrics"] = lyrArr
+	df.to_csv(songArtistCSV[:-4]+" lyrics.csv")
+
+saveCSV("test.csv")
